8c2ac9c727eaf3b1d99d40384401fa51cb50830e
Kamil Zegadlo <KamilZegadlo@gmail.com>
2020-02-22 00:54:11 +0000

    Add project files.

---
+++ LinqExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
